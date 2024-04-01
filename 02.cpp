b37266ed0e3dd9d6e752c703533ce1b389e5d220
teatree32 <164837312+teatree32@users.noreply.github.com>
2024-04-01 20:20:11 +0900

    [C++] SWING 1-1 스터디 3주차

--- 02.cpp
+++ 02.cpp
@@ -1,187 +1,72 @@
 #include <iostream>
 using namespace std;
 
-
-int num = 0; //영화 예약 시스템 선택 변수
-int a = 0; //좌석 선택시 행을 가르키는 변수
-int b = 0; //좌석 선택시 열을 가르키는 변수
-int seat[10][6] = { {0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0} };//배열 초기화
-int x, y = 0; //인원수 변수 성인:x, 청소년:y
-int z = 0; //바꿀 좌석 개수를 가르키는 변수
-int sum_x = 0; //누적된 성인 인원수 변수
-int sum_y = 0; //누적된 청소년 인원수 변수
+int num = 0; //라면 번호 변수
+char a; // Y/N 구분 변수
+int b = 0; // 라면 개수 변수
+int totalprice = 0; //누적된 라면 가격 변수
 
 
 
-int book() { //메뉴 함수 정의
+int order() { //메뉴 함수 정의
 
-	cout << "**영화 예약 시스템**\n";
-	cout << "1. 좌석 예약 시스템 \n2.예약 변경 \n3.프로그램 종료\n";
-	cout << "번호를 입력하세요 : ";
+	cout << "***** 슈니의 라면가게에 오신 것을 환영합니다 *****" << endl << endl;
+	cout << "1. 일반 라면 (3500원)" << endl;
+	cout << "2. 김치 라면 (4000원)" << endl;
+	cout << "3. 치즈 라면 (4200원) " << endl;
+	cout << "4. 떡 라면 (4300원)" << endl;
+	cout << "5. 떡 만두 라면 (4500원)" << endl << endl;
+	cout << "구매하실 라면 번호를 입력해 주세요(종료는 0) : ";
 	cin >> num;
 	return 0;
 }
 
+int total(int* sum, int* p) {//라면 가격 구하는 함수 정의
+	int sumsum = (*sum * *p);
+	return sumsum;
+}
+
 int main() {
+	
+	string menu[] = { "일반 라면", "김치 라면", "치즈 라면", "떡 라면", "떡 만두 라면" };
+	int price[] = { 3500, 4000, 4200, 4300, 4500 };
+	int* sum = NULL;
+	sum = price; // 라면 가격 포인터
+	int* p = &b; //라면 개수 포인터
+
+	
 	while (1) {
-		book(); //메뉴 출력
+		order(); //메뉴 출력
+		
 
-		if (num == 1) { //1. 좌석 예약시스템
-			cout << endl << endl;//예약가능 좌석 보여주기
-			cout << "1 2 3 4 5 6" << endl;
-			cout << "-----------" << endl;
-			cout << endl;
-
-			for (int a = 0; a < 10; ++a) {
-				for (int b = 0; b < 6; ++b) {
-					cout << seat[a][b] << " ";
-				}
-				cout << endl;
-
-			}
-			cout << endl << "성인(14000원): ";
-			cin >> x;
-			sum_x += x; //입력된 x값을 누적하여 총 성인 인원수를 구함
-
-			cout << endl << "청소년(11000원): ";
-			cin >> y;
-			sum_y += y; //입력된 y값을 누젛가여 총 청소년 인원수를 구함
-
-
-
-
-			for (int i = 0; i < x + y; ++i) { // 입력한 성인 인원수와 청소년 인원수 만큼 성공할 때까지 반복
-				cout << endl << "몇열, 몇번째 좌석을 예약하시겠습니까?" << endl;
-
-
+		if (num < 6 && num > 0) {//1~5 라면 선택할 때
+			cout << menu[num-1] << "을(를) 구매하시겠습니까?(Y/N) : ";
+			cin >> a;
+			if (a == 'Y') {//Y를 입력할 때
+				cout << "구매하실 라면 개수를 입력해 주세요 : ";
 				cin >> b;
-				cin >> a;
-
-				cout << endl;
-				if (a < 11 && b < 7) { //행과 열을 범위에 맞게 입력했을때
-					if (seat[a - 1][b - 1] == 0) { //빈자리라면 예약
-						cout << "예약되었습니다." << endl;
-						seat[a - 1][b - 1] = 1; //예약좌석으로 바꾸기
-
-					}
-					else { // 이미 예약된 좌석일때
-						cout << "이미 예약되었습니다. 다른 자리를 선택하세요" << endl;
-						i = i - 1;
-					}
-				}
-				else { //행과 열을 범위에 맞지 않게 입력했을때
-					cout << "예약가능한 좌석이 아닙니다." << endl;
-					i = i - 1;
-				}
+				cout << menu[num-1] << b << "개를 구매하셨습니다. " << endl;
+				cout << "가격은 " << total(&price[num-1], &b) << "원 입니다." << endl << endl;
+				totalprice += total(&price[num-1], &b);
 			}
-			cout << endl << endl; //예약을 성공하면 예약한 좌석 보여줌
-			cout << "1 2 3 4 5 6" << endl;
-			cout << "-----------" << endl;
-			cout << endl;
-
-			for (int a = 0; a < 10; ++a) {
-				for (int b = 0; b < 6; ++b) {
-					cout << seat[a][b] << " ";
-				}
-				cout << endl;
-
+			else if (a == 'N') {//N을 입력할 때
+				continue;
 			}
-			cout << endl;
-
-		}
-		else if (num == 2) { //2.예약 변경
-			cout << endl << endl; //예약된 좌석을 보여줌
-			cout << "1 2 3 4 5 6" << endl;
-			cout << "-----------" << endl;
-			cout << endl;
-
-			for (int a = 0; a < 10; ++a) {
-				for (int b = 0; b < 6; ++b) {
-					cout << seat[a][b] << " ";
-				}
-				cout << endl;
-
+			else { //Y/N 이외에 다른것을 입력할 때
+				cout << "잘못된 입력입니다." << endl;
 			}
-			cout << "바꿀 좌석의 갯수를 입력하세요 : ";
-			cin >> z;
-
-			for (int o = 0; o < z; ++o) { //변경할 좌석수만큼 성공할때까지 반복
-				cout << "현재 좌석과 바꿀 좌석을 입력하세요" << endl;
-				cout << "현재 좌석(N열, M번째): ";
-				cin >> b;
-				cin >> a;
-
-				if (a < 11 && b < 7) {//현재좌석을 범위에 맞게 입력했을 때
-					if (seat[a - 1][b - 1] == 1) {//현재 좌석을 알맞게 입력했을 때
-						seat[a - 1][b - 1] = 0; //현재 좌석을 빈자리 바꿈
-						cout << "변경 좌석(n열, m번째): ";
-
-						int c = a - 1; //현재 좌석 행,열값 저장
-						int d = b - 1;
-
-						cin >> b;
-						cin >> a;
-
-						if (a < 11 && b < 7) { //변경 좌석을 범위에 맞게 입력했을 때
-							if (seat[a - 1][b - 1] == 0) {//빈자리면 변경
-								cout << "변경되었습니다." << endl;
-								seat[a - 1][b - 1] = 1; //예약좌석으로 바꾸기
-							}
-							else {//빈자리가 아닐때
-								cout << "이미 예약되었습니다. 다른 좌석을 선택하세요" << endl;
-								seat[c][d] = 1; //저장한 현재 좌석을 다시 예약된 좌석으로 변경
-								o = o - 1;
-							}
-						}
-						else {//변경 좌석을 범위에 맞지 않게 입력했을 때
-							cout << "좌석 번호를 다시 입력해주세요" << endl;
-							seat[c][d] = 1; //저장한 현재 좌석을 다시 예약된 좌석으로 변경
-							o = o - 1;
-
-						}
-
-
-
-					}
-					else {//현재 좌석을 알맞지 않게 입력했을 때
-						cout << "현재 좌석이 틀렸습니다. 다시 입력해주세요" << endl;
-						o = o - 1;
-					}
-				}
-				else {// 현재 좌석을 범위에 맞지 않게 입력했을 때
-					cout << "좌석 번호를 다시 입력해주세요" << endl;
-					o = o - 1;
-				}
-			}
-			cout << endl << endl; //변경된 좌석 보여주기
-			cout << "1 2 3 4 5 6" << endl;
-			cout << "-----------" << endl;
-			cout << endl;
-
-			for (int a = 0; a < 10; ++a) {
-				for (int b = 0; b < 6; ++b) {
-					cout << seat[a][b] << " ";
-				}
-				cout << endl;
-
-			}
-
 		}
 
-		else if (num == 3) { //3. 프로그램 종료
-			int total = 14000 * sum_x + 11000 * sum_y; //총 성인 인원수에 14000원 곱하고 총 청소년 인원수에 11000원곱해서 더한 총 금액 변수
-			cout << "총" << total << "원 입니다."; //총 금액 출력
+		else if (num == 0) {//0을 이력할 때
+
+			cout << "총 가격은 " <<  totalprice << "원 입니다." << endl;
 			return 0;
-		}
-
-		else {//1,2,3 외에 다른 숫자를 입력했을 때
-			cout << "올바른 숫자를 입력해주세요" << endl << endl;
 
 		}
 
+		else {//0~5이외에 다른것을 입력할 대
+			
+			cout << "제대로 된 숫자를 입력해주세요." << endl << endl;
+		}
 	}
-
-
-
-
 }
